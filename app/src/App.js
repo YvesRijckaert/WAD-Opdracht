0c336b4802453cbc405072ab28d4a0e78a4bc528
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-03-12 19:04:28 +0100

    Everything needs to be in an work day object (I think)

--- app/src/App.js
+++ app/src/App.js
@@ -42,52 +42,12 @@ class App extends Component {
   };
 
   render() {
-    const { name, location, start, end, workDay } = this.state;
-    const total = calculateTotal(parseFloat(start), parseFloat(end));
+    const { workDay } = this.state;
     return (
       <div className="App">
-        <form>
-          <Input
-            value={name}
-            type="text"
-            name="Name:"
-            onChange={e => this.handleInputChange("name", e)}
-          />
-          <Input
-            value={location}
-            type="text"
-            name="Location:"
-            onChange={e => this.handleInputChange("location", e)}
-          />
-          <Input
-            value={start}
-            type="number"
-            name="Start hour:"
-            min="1"
-            max="24"
-            onChange={e => this.handleInputChange("start", e)}
-          />
-          <Input
-            value={end}
-            type="number"
-            name="End hour:"
-            min="1"
-            max="24"
-            onChange={e => this.handleInputChange("end", e)}
-          />
-        </form>
-        <HourObject
-          name={name}
-          location={location}
-          startHour={start}
-          endHour={end}
-        />
-        {
-        Object.keys(workDay).map(id => (
-        <li key={id}>{workDay[id].value}</li>
-        ))
-        }
-        <TotalHours totalHours={total} totalAmount="€ 96" />
+      <AddHoursForm workDay={workDay} />
+        <HourObject workDay={workDay} />
+        <TotalHours totalHours="test" totalAmount="€ 96" />
       </div>
     );
   }
